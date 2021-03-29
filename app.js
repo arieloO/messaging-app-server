7b666839aa7e586708dfeb9d8f6271324a048017
Ariel <ariel.levyk@gmail.com>
2021-03-29 16:59:08 +0200

    copy from messaging-app server folder

---
+++ app.js
@@ -0,0 +1,68 @@
+const app = require("express")();
+
+const http = require("http").createServer(app);
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:3000",
+    method: ["GET", "POST"],
+  },
+});
+
+// get
+app.get("/", (req, res) => {
+  // sends html file as response
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  let username = null;
+  // user connects
+  console.log("a user connected");
+
+  // broadcast userX is connected);
+  socket.on("user connection", (name) => {
+    console.log("emit login : ", name);
+    username = name;
+    io.emit("user log", {
+      type: "login",
+      name: name,
+      message: `welcome ${name} !`,
+    });
+  });
+
+  // user disconnects
+  socket.on("disconnect", () => {
+    console.log("user disconnected : ", username);
+    io.emit("user log", {
+      type: "logout",
+      name: username,
+      message: `${username} left the conversation...`,
+    });
+  });
+
+  // socket.on("user disconnect", (name) => {
+  //   console.log("a user disconnected");
+  //   console.log(name, " has logged out");
+  //   io.emit("user log", {
+  //     type: "logout",
+  //     name: name,
+  //     message: `${name} left the conversation...`,
+  //   });
+  // });
+
+  // broadcast message
+  socket.on("chat message", ({ type, name, message }) => {
+    console.log(name + ": " + message);
+    io.emit("chat message", { type, name, message });
+  });
+
+  // broadcast user is typing
+  socket.on("isTyping", (name) => {
+    console.log(name, " is typing");
+    io.emit("user isTyping", name);
+  });
+});
+
+http.listen(4040, () => {
+  console.log("The application is running on localhost:4040!");
+});
